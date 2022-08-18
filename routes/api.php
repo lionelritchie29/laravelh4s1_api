8f0f2c320d2048ddd44c53648699d6db5b5d3359
lionelritchie29 <lionelrtchieee@gmail.com>
2022-08-18 09:12:45 +0700

    feat: add crud api routes and passport authentication

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +17,52 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// route nya di auto prefix pake /api
+Route::prefix('products')->group(function() {
+    Route::get('/', function() {
+        $products = Product::all();
+        return ['data' => ProductResource::collection(($products))];
+    });
+
+    Route::post('/', function(Request $request) {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'price' => 'required|integer',
+            'description' => 'required|string',
+            'category_id' => 'required|exists:categories,id'
+        ]);
+
+        return Product::create($data);
+    })->middleware('auth:api');
+
+    Route::put('/', function(Request $request) {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'price' => 'required|integer',
+            'description' => 'required|string',
+            'category_id' => 'required|exists:categories,id',
+            'id' => 'required|exists:products'
+        ]);
+
+        return Product::find($data['id'])->update($data);
+    });
+
+    Route::delete('/{id}', function($id) {
+        return Product::find($id)->delete();
+    }); 
+});
+
+
+Route::post('/login', function(Request $request) {
+    $data = $request->validate([
+        'email' => 'required|string',
+        'password' => 'required|string'
+    ]);
+
+    if (Auth::attempt($data)) {
+        $user = Auth::user();
+        return ['token' => $user->createToken('access_token')->accessToken];
+    } else {
+        return ['token' => null];
+    }
 });
